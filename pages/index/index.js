7aac63348bfe45b01f7a8930d88513fd471c63d1
Peyton <18811451267@163.com>
2018-05-08 21:52:29 +0800

    title

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,27 @@
 //index.js
-//获取应用实例
-const app = getApp()
+
+const newsType = {'gn':'国内','gj':'国际','cj':'财经','yl':'娱乐','js':'军事','ty':'体育','other':'其他'}
+
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  data:{
+    titleListItem: newsType
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+
+
+
+  getNewsList(newsType){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: newsType 
+      },
+      success: res => {
+      },
+      complete: () => {
+        callback && callback()
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+   })
   }
+
 })
