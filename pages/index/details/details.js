6292cf1500af61d522cdd57a31cfd93a7ceaba34
Peyton <18811451267@163.com>
2018-05-16 00:39:51 +0800

    complete

--- pages/index/details/details.js
+++ pages/index/details/details.js
@@ -1,4 +1,44 @@
 // pages/index/details/details.js
 Page({
-  
+  data:{
+    title:"",
+    source:"",
+    readCount:"",
+    time:"",
+    newsDetailBody:[]
+  },
+
+  //加载详情页面
+  onLoad(options){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/detail',
+      data: {
+        id: options.id
+      },
+      success : res=>{
+          this.setNewsDetailBody(res.data.result)
+      }
+    })
+  },
+
+  //赋值详情页面
+  setNewsDetailBody(result) {
+    let content=result.content
+    console.log("content.length " + content.length)
+    let newsDetailBody = []
+    for (let i = 0; i < content.length; i++) {
+      console.log(content[i])
+      newsDetailBody.push({
+        type: content[i].type,
+        text: content[i].type === 'image' ? content[i].src : content[i].text,
+      })
+    }
+    this.setData({
+      title: result.title,
+      source: result.source === "" ? "未知来源" : result.source,
+      time: result.date.substr(11, 5),
+      readCount: "阅读 " + result.readCount,
+      newsDetailBody: newsDetailBody
+    })
+  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -23,13 +23,13 @@ Page({
     this.getNewsList()
   },
   
-  //获取新闻列表
+  //点击标题栏更新新闻列表
   getNewsList: function (e) {
-      //判断是否第一次，如果是则跳过
-       if(e){
+    //判断是否第一次，如果是则跳过
+      if(e){
         var dataset = e.target.dataset;
         var idx = e.target.dataset.idx;
-
+        
         for (var i in newsType) {
           console.log("i " + i + " idx " + idx)
           if(i!==idx)
@@ -43,7 +43,12 @@ Page({
           console.log("i " + newsType[i].select)
         }
       }
+      this.requestNewsList()
+  },
 
+  //获取请求新闻列表
+  requestNewsList(callback)
+  {
     //请求url
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
@@ -56,37 +61,45 @@ Page({
         this.setData({
           titleListItem : newsType
         })
+      },
+      complete: () => {
+        callback && callback()
       }
     })
-    
   },
 
+  //赋值新闻列表
   setNewsList(result){
     console.log("setNewsList " + result.length)
     let newsList=[]
     for (let i = 0; i < result.length; i++){
       newsList.push({
+        id:result[i].id,
         text: result[i].title,
         source: result[i].source === "" ? "未知来源" : result[i].source,
         time: result[i].date.substring(11,16),
         iconPath: result[i].firstImage
       })
     }
-    
     this.setData({
       newsList: newsList
     })
-
-    console.log("newsList" + this.data.newsList[0].text)
+    console.log("newsList" + this.data.newsList[0].id)
   },
 
 //切换到第二个页面
-  onTabNewsDetails() {
-    console.log("onTabNewsDetails")
+  onTabNewsDetails(e) {
+    console.log("onTabNewsDetails " + e.currentTarget.dataset.id)
     wx.showToast("onTabNewsDetails")
     wx.navigateTo({
-      url: '/pages/index/details/details'
+      url: '/pages/index/details/details?id=' + e.currentTarget.dataset.id
     })
-  }
-  
+  },
+
+//下拉刷新
+  onPullDownRefresh: function () {
+    this.requestNewsList(()=>{
+    wx.stopPullDownRefresh()
+    })
+    }
 })
